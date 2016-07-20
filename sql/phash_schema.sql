8e1f3bdbe993d12ec24d2b45cbb1173d27893997
Andrew Dolgov <noreply@fakecake.org>
2016-07-20 19:23:04 +0300

    update schema

--- sql/phash_schema.sql
+++ sql/phash_schema.sql
@@ -4,7 +4,7 @@ create table ttrss_plugin_img_phash_urls(
 	id serial not null,
 	article_guid varchar(250) not null,
 	url text not null,
-	owner_uid integer not null references ttrss_users(id),
+	owner_uid integer not null references ttrss_users(id) on delete CASCADE,
 	phash bigint,
 	created_at timestamp not null default NOW());
 
