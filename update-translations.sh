ad71ab3dac3e0dac33b1de4ead33592fdd38f062
Andrew Dolgov <noreply@fakecake.org>
2019-03-05 10:01:36 +0300

    add I18N skeleton

---
+++ update-translations.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+TEMPLATE=af_zz_img_phash.pot
+
+xgettext -kP__:2 -kP__sprintf:2 -L PHP -o $TEMPLATE *.php
+xgettext -k__ -L Java -j -o $TEMPLATE *.js
+
+update_lang() {
+	if [ -f $1.po ]; then
+		msgmerge --no-wrap --width 1 -U $1.po $TEMPLATE
+		msgfmt --statistics $1.po -o $1.mo
+	else
+		echo "Usage: $0 [-p|<basename>]"
+	fi
+}
+
+LANGS=`find locale -name 'af_zz_img_phash.po'`
+
+for lang in $LANGS; do
+	echo Updating $lang...
+	PO_BASENAME=`echo $lang | sed s/.po//`
+	update_lang $PO_BASENAME
+done
